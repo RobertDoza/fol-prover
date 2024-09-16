1b1cff24769132c9ff1ccb5194d7a681aa156e9d
Robert <dozarobert98@gmail.com>
2024-09-16 18:18:01 +0200

    Define term classes

--- formula.hpp
+++ formula.hpp
@@ -1,13 +1,42 @@
 #pragma once
 
+#include <string>
+#include <vector>
+#include <memory>
+
 // Base formula class
 class Formula {};
 
 // Term
 class Term {};
-class Variable : public Term {};
-class Constant : public Term {};
-class ComplexTerm : public Term {};
+
+class Variable : public Term {
+	public:
+		Variable(const std::string& name)
+			:_name(name)
+		{}
+	private:
+		std::string _name;
+};
+
+class Constant : public Term {
+	public:
+		Constant(const std::string& name)
+			:_name(name)
+		{}
+	private:
+		std::string _name;
+};
+
+class ComplexTerm : public Term {
+	public:
+		ComplexTerm(const std::string& f, const std::vector<std::shared_ptr<Term>>& terms)
+			:_function_symbol(f), _subterms(terms)
+		{}
+	private:
+		std::string _function_symbol;
+		std::vector<std::shared_ptr<Term>> _subterms;
+};
 
 // Atomic Formula
 class AtomicFormula : public Formula {};
