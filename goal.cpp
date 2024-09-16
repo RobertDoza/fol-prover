95012fc46d5d355f7f9f762a4267aad84a424f7e
Robert <dozarobert98@gmail.com>
2024-09-17 00:08:49 +0200

    Implement constructor and to_string method for Goal class

--- goal.cpp
+++ goal.cpp
@@ -1,2 +1,23 @@
 #include "goal.hpp"
 
+std::string Goal::to_string() const {
+	std::stringstream s;
+	
+	s << "[";
+	
+	for (size_t i = 0; i < _assumptions.size(); i++) {
+		s << *_assumptions[i];
+		if (i < _assumptions.size() - 1) {
+			s << ", ";
+		}
+	}
+	
+	s << "]";
+	
+	s << " ⊢ ";
+	
+	s << *_goal;
+	
+	return s.str();
+}
+
--- goal.hpp
+++ goal.hpp
@@ -4,6 +4,10 @@
 
 class Goal {
 	public:
+		Goal(const std::vector<std::shared_ptr<Formula>>& a, const std::shared_ptr<Formula>& g)
+			:_assumptions(a), _goal(g)
+		{}
+		
 		std::string to_string() const;
 	private:
 		std::vector<std::shared_ptr<Formula>> _assumptions;
