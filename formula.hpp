59b4e7f94850bdc0fa36d39370c2b3316c67b41f
Robert <dozarobert98@gmail.com>
2024-09-16 17:40:38 +0200

    Add formula classes

--- formula.hpp
+++ formula.hpp
@@ -1,2 +1,21 @@
 #pragma once
 
+class Formula {};
+
+class AtomicFormula : public Formula {};
+
+class LogicalConnective : public Formula {};
+
+class UnaryConnective : public LogicalConnective {};
+class Negation : public UnaryConnective {};
+
+class BinaryConnective : public LogicalConnective {};
+class Conjunction : public BinaryConnective {};
+class Disjunction : public BinaryConnective {};
+class Implication : public BinaryConnective {};
+class Equivalence : public BinaryConnective {};
+
+class Quantifier : public Formula {};
+class ForAll : public Quantifier {};
+class Exists : public Quantifier {};
+
