bfef40783f44dbf879c5254769d17214e34435f4
Robert <dozarobert98@gmail.com>
2024-09-20 15:33:58 +0200

    Move term classes from formula.hpp to term.hpp

--- formula.hpp
+++ formula.hpp
@@ -5,6 +5,8 @@
 #include <memory>
 #include <sstream>
 
+#include "term.hpp"
+
 enum class FormulaType {
 	True,
 	False,
@@ -19,12 +21,6 @@ enum class FormulaType {
 	Exists
 };
 
-enum class TermType {
-	Variable,
-	Constant,
-	ComplexTerm
-};
-
 // Base formula class
 class Formula {
 	public:
@@ -36,57 +32,6 @@ class Formula {
 bool are_equal(const std::shared_ptr<Formula>& f1, const std::shared_ptr<Formula>& f2);
 
 std::ostream& operator<<(std::ostream& out, const Formula& formula);
-
-// Term
-class Term {
-	public:
-		virtual std::string to_string() const = 0;
-		virtual TermType type() const = 0;
-};
-
-bool are_equal(const std::shared_ptr<Term>& t1, const std::shared_ptr<Term>& t2);
-
-std::ostream& operator<<(std::ostream& out, const Term& term);
-
-class Variable : public Term {
-	public:
-		Variable(const std::string& name)
-			:_name(name)
-		{}
-		
-		std::string to_string() const override;
-		TermType type() const override;
-		bool operator==(const Variable& other) const;
-	private:
-		std::string _name;
-};
-
-class Constant : public Term {
-	public:
-		Constant(const std::string& name)
-			:_name(name)
-		{}
-		
-		std::string to_string() const override;
-		TermType type() const override;
-		bool operator==(const Constant& other) const;
-	private:
-		std::string _name;
-};
-
-class ComplexTerm : public Term {
-	public:
-		ComplexTerm(const std::string& f, const std::vector<std::shared_ptr<Term>>& terms)
-			:_function_symbol(f), _subterms(terms)
-		{}
-		
-		std::string to_string() const override;
-		TermType type() const override;
-		bool operator==(const ComplexTerm& other) const;
-	private:
-		std::string _function_symbol;
-		std::vector<std::shared_ptr<Term>> _subterms;
-};
 
 // Atomic Formula
 class AtomicFormula : public Formula {
---
+++ term.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+enum class TermType {
+	Variable,
+	Constant,
+	ComplexTerm
+};
+
+class Term {
+	public:
+		virtual std::string to_string() const = 0;
+		virtual TermType type() const = 0;
+};
+
+bool are_equal(const std::shared_ptr<Term>& t1, const std::shared_ptr<Term>& t2);
+
+std::ostream& operator<<(std::ostream& out, const Term& term);
+
+class Variable : public Term {
+	public:
+		Variable(const std::string& name)
+			:_name(name)
+		{}
+		
+		std::string to_string() const override;
+		TermType type() const override;
+		bool operator==(const Variable& other) const;
+	private:
+		std::string _name;
+};
+
+class Constant : public Term {
+	public:
+		Constant(const std::string& name)
+			:_name(name)
+		{}
+		
+		std::string to_string() const override;
+		TermType type() const override;
+		bool operator==(const Constant& other) const;
+	private:
+		std::string _name;
+};
+
+class ComplexTerm : public Term {
+	public:
+		ComplexTerm(const std::string& f, const std::vector<std::shared_ptr<Term>>& terms)
+			:_function_symbol(f), _subterms(terms)
+		{}
+		
+		std::string to_string() const override;
+		TermType type() const override;
+		bool operator==(const ComplexTerm& other) const;
+	private:
+		std::string _function_symbol;
+		std::vector<std::shared_ptr<Term>> _subterms;
+};
+
