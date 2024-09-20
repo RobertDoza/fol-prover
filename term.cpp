f98dfe7bf4453d1c50389079d95bd8b2b137b10b
Robert <dozarobert98@gmail.com>
2024-09-20 23:34:02 +0200

    Add const qualifier to method Term::rename_var()

--- term.cpp
+++ term.cpp
@@ -59,7 +59,7 @@ std::shared_ptr<Term> Variable::replace(const std::string& var_name, const std::
 	}
 }
 
-std::shared_ptr<Term> Variable::rename_var(const std::string& old_name, const std::string& new_name) {
+std::shared_ptr<Term> Variable::rename_var(const std::string& old_name, const std::string& new_name) const {
 	auto var = std::make_shared<Variable>(*this);
 	
 	if (_name == old_name) {
@@ -92,7 +92,7 @@ std::shared_ptr<Term> Constant::replace(const std::string& var_name, const std::
 	return std::make_shared<Constant>(*this);
 }
 
-std::shared_ptr<Term> Constant::rename_var(const std::string& old_name, const std::string& new_name) {
+std::shared_ptr<Term> Constant::rename_var(const std::string& old_name, const std::string& new_name) const {
 	(void) old_name;
 	(void) new_name;
 	return std::make_shared<Constant>(*this);
@@ -160,7 +160,7 @@ std::shared_ptr<Term> ComplexTerm::replace(const std::string& var_name, const st
 	return std::make_shared<ComplexTerm>(_function_symbol, new_subterms);
 }
 
-std::shared_ptr<Term> ComplexTerm::rename_var(const std::string& old_name, const std::string& new_name) {
+std::shared_ptr<Term> ComplexTerm::rename_var(const std::string& old_name, const std::string& new_name) const {
 	std::vector<std::shared_ptr<Term>> new_subterms = {};
 	
 	for (const auto& t : _subterms) {
