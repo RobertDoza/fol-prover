9956afdc315e64c0398426bdcf6bf318a8461be0
Robert <dozarobert98@gmail.com>
2024-09-20 17:37:43 +0200

    Move atomic formula classes from Formula module to AtomicFormula module

---
+++ atom.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "formula.hpp"
+
+class AtomicFormula : public Formula {
+	public:
+		virtual std::set<std::string> get_variable_names() const = 0;
+		std::set<std::string> get_free_variable_names() const override;
+	protected:
+		bool requires_parentheses() const override;
+};
+
+class LogicalConstant : public AtomicFormula {
+	public:
+		std::set<std::string> get_variable_names() const override;
+};
+
+class True : public LogicalConstant {
+	public:
+		std::string to_string() const override;
+		FormulaType type() const override;
+		bool operator==(const True& other) const;
+};
+
+class False : public LogicalConstant {
+	public:
+		std::string to_string() const override;
+		FormulaType type() const override;
+		bool operator==(const False& other) const;
+};
+
+class Atom : public AtomicFormula {
+	public:
+		Atom(const std::string& p)
+			:_predicate_symbol(p)
+		{}
+	protected:
+		std::string _predicate_symbol;
+};
+
+class SimpleAtom : public Atom {
+	public:
+		SimpleAtom(const std::string& p)
+			:Atom(p)
+		{}
+		
+		std::string to_string() const override;
+		FormulaType type() const override;
+		bool operator==(const SimpleAtom& other) const;
+		std::set<std::string> get_variable_names() const override;
+};
+
+class ComplexAtom : public Atom {
+	public:
+		ComplexAtom(const std::string& p, const std::vector<std::shared_ptr<Term>>& terms)
+			:Atom(p), _terms(terms)
+		{}
+		
+		std::string to_string() const override;
+		FormulaType type() const override;
+		bool operator==(const ComplexAtom& other) const;
+		std::set<std::string> get_variable_names() const override;
+	private:
+		std::vector<std::shared_ptr<Term>> _terms;
+};
+
--- formula.cpp
+++ formula.cpp
@@ -1,4 +1,5 @@
 #include "formula.hpp"
+#include "atom.hpp"
 
 bool are_equal(const std::shared_ptr<Formula>& f1, const std::shared_ptr<Formula>& f2) {
 	FormulaType type = f1->type();
@@ -71,112 +72,6 @@ bool are_equal(const std::shared_ptr<Formula>& f1, const std::shared_ptr<Formula
 std::ostream& operator<<(std::ostream& out, const Formula& formula) {
 	out << formula.to_string();
 	return out;
-}
-
-std::set<std::string> AtomicFormula::get_free_variable_names() const {
-	return get_variable_names();
-}
-
-bool AtomicFormula::requires_parentheses() const {
-	return false;
-}
-
-std::set<std::string> LogicalConstant::get_variable_names() const {
-	return {};
-}
-
-std::string True::to_string() const {
-	return "⊤ ";
-}
-
-FormulaType True::type() const {
-	return FormulaType::True;
-}
-
-bool True::operator==(const True& other) const {
-	(void) other;
-	return true;
-}
-
-std::string False::to_string() const {
-	return "⊥ ";
-}
-
-FormulaType False::type() const {
-	return FormulaType::False;
-}
-
-bool False::operator==(const False& other) const {
-	(void) other;
-	return true;
-}
-
-std::string SimpleAtom::to_string() const {
-	return _predicate_symbol;
-}
-
-FormulaType SimpleAtom::type() const {
-	return FormulaType::SimpleAtom;
-}
-
-bool SimpleAtom::operator==(const SimpleAtom& other) const {
-	return this->_predicate_symbol == other._predicate_symbol;
-}
-
-std::set<std::string> SimpleAtom::get_variable_names() const {
-	return {};
-}
-
-std::string ComplexAtom::to_string() const {
-	std::stringstream s;
-	
-	s << _predicate_symbol;
-	
-	s << "(";
-	
-	for (size_t i = 0; i < _terms.size(); i++) {
-		s << _terms[i]->to_string();
-		if (i < _terms.size() - 1) {
-			s << ", ";
-		}
-	}
-	
-	s << ")";
-	
-	return s.str();
-}
-
-bool ComplexAtom::operator==(const ComplexAtom& other) const {
-	if (this->_predicate_symbol != other._predicate_symbol) {
-		return false;
-	}
-	
-	if (this->_terms.size() != other._terms.size()) {
-		return false;
-	}
-	
-	for (size_t i = 0; i < this->_terms.size(); ++i) {
-		if (!are_equal(this->_terms[i], other._terms[i])) {
-			return false;
-		}
-	}
-	
-	return true;
-}
-
-FormulaType ComplexAtom::type() const {
-	return FormulaType::ComplexAtom;
-}
-
-std::set<std::string> ComplexAtom::get_variable_names() const {
-	std::set<std::string> variable_names = {};
-
-	for (const auto& t : _terms) {
-		auto sub_vars = t->get_variable_names();
-		variable_names.insert(sub_vars.begin(), sub_vars.end());
-	}
-	
-	return variable_names;
 }
 
 bool LogicalConnective::requires_parentheses() const {
