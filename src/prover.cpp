95a1ecfc02fed241b7f221db7ad97643c560ecef
Robert <dozarobert98@gmail.com>
2024-09-23 20:15:00 +0200

    Implement Prover::parse_user_input()
    - Began work on Prover::interact_with_user().

--- src/prover.cpp
+++ src/prover.cpp
@@ -2,10 +2,24 @@
 
 #include <iostream>
 
+// TODO: move to a more appropriate place
+std::vector<std::string> split_into_words(const std::string& input) {
+    std::vector<std::string> words;
+    std::istringstream stream(input);
+    std::string word;
+    
+    while (stream >> word) {
+        words.push_back(word);
+    }
+    
+    return words;
+}
+
 void Prover::start() {
 	print_intro_message();
 	
 	// TODO...
+	interact_with_user();
 }
 
 void Prover::print_intro_message() {
@@ -13,7 +27,114 @@ void Prover::print_intro_message() {
 	std::cout << commands << std::endl;
 }
 
+Command Prover::parse_user_input(const std::string& user_input) {
+	std::vector<std::string> words = split_into_words(user_input);
+	
+	auto length = words.size();
+	
+	if (length == 0) {
+		return {CommandType::Empty, {}};
+	}
+	
+	std::string command_word = words[0];
+	
+	if (command_word == "apply") {
+		if (length == 1 or length > 3) {
+			return {CommandType::UnknownCommand, {}};
+		}
+		
+		if (words[1] == "assumption") {
+			if (length > 2) {
+				return {CommandType::UnknownCommand, {}};
+			}
+		
+			return {CommandType::RuleApplication, {Rule::Assumption}};
+		}
+		
+		if (words[1] == "rule") {
+			if (length == 2) {
+				return {CommandType::UnknownCommand, {}};
+			}
+			
+			if (words[2] == "notI") {
+				return {CommandType::RuleApplication, {Rule::NotI}};
+			} else if (words[2] == "conjI") {
+				return {CommandType::RuleApplication, {Rule::ConjI}};
+			} else if (words[2] == "disjI1") {
+				return {CommandType::RuleApplication, {Rule::DisjI1}};
+			} else if (words[2] == "disjI2") {
+				return {CommandType::RuleApplication, {Rule::DisjI2}};
+			} else if (words[2] == "impI") {
+				return {CommandType::RuleApplication, {Rule::ImpI}};
+			} else if (words[2] == "iffI") {
+				return {CommandType::RuleApplication, {Rule::IffI}};
+			} else if (words[2] == "allI") {
+				return {CommandType::RuleApplication, {Rule::AllI}};
+			} else if (words[2] == "exI") {
+				return {CommandType::RuleApplication, {Rule::ExI}};
+			}
+			
+			return {CommandType::UnknownCommand, {}};
+		}
+		
+		if (words[1] == "erule") {
+			if (length == 2) {
+				return {CommandType::UnknownCommand, {}};
+			}
+			
+			if (words[2] == "notE") {
+				return {CommandType::RuleApplication, {Rule::NotE}};
+			} else if (words[2] == "conjE") {
+				return {CommandType::RuleApplication, {Rule::ConjE}};
+			} else if (words[2] == "disjE") {
+				return {CommandType::RuleApplication, {Rule::DisjE}};
+			} else if (words[2] == "impE") {
+				return {CommandType::RuleApplication, {Rule::ImpE}};
+			} else if (words[2] == "iffE") {
+				return {CommandType::RuleApplication, {Rule::IffE}};
+			} else if (words[2] == "allE") {
+				return {CommandType::RuleApplication, {Rule::AllE}};
+			} else if (words[2] == "exE") {
+				return {CommandType::RuleApplication, {Rule::ExE}};
+			}
+			
+			return {CommandType::UnknownCommand, {}};
+		}
+		
+		return {CommandType::UnknownCommand, {}};
+	} else if (command_word == "exit") {
+		if (length > 1) {
+			return {CommandType::UnknownCommand, {}};
+		}
+		
+		return {CommandType::ExitRequest, {}};
+	} else if (command_word == "list") {
+		if (length > 1) {
+			return {CommandType::UnknownCommand, {}};
+		}
+		
+		return {CommandType::ListRequest, {}};
+	} else if (command_word == "help") {
+		if (length > 1) {
+			return {CommandType::UnknownCommand, {}};
+		}
+		
+		return {CommandType::HelpRequest, {}};
+	}
+	
+	return {CommandType::UnknownCommand, {}};
+}
+
 void Prover::interact_with_user() {
-	// TODO
+	std::string user_input;
+
+	while (true) {
+		std::getline(std::cin, user_input);
+		
+		Command command = parse_user_input(user_input);
+		(void) command;
+		
+		// TODO
+	}
 }
 
