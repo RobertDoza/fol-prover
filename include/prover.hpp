290d0e1b4fd6742b06994dd223f361f2e1371e57
Robert <dozarobert98@gmail.com>
2024-09-23 18:52:33 +0200

    Implement Prover class constructor; add Prover::start(), Prover::interact_with_user()

--- include/prover.hpp
+++ include/prover.hpp
@@ -6,10 +6,14 @@
 class Prover {
 	public:
 		Prover(const std::shared_ptr<Formula>& f)
-			:_formula_to_prove(f) //, _goal_keeper(f) TODO
+			:_formula_to_prove(f), _goal_keeper(f)
 		{}
+		
+		void start();
+	private:
+		void interact_with_user();
 	private:
 		std::shared_ptr<Formula> _formula_to_prove;
-		// GoalKeeper _goal_keeper; TODO
+		GoalKeeper _goal_keeper;
 };
 
