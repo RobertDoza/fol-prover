26a62ee88df9624c56cdcfc9de3950336ac76fe2
Robert <dozarobert98@gmail.com>
2024-09-23 18:56:41 +0200

    Add string constants to Prover class for user output
    - Added a 'commands' string with available commands for the user.
    - Added a 'list_of_rules' string that lists the avaiable rules of natural deduction.

--- include/prover.hpp
+++ include/prover.hpp
@@ -3,6 +3,33 @@
 #include "formula.hpp"
 #include "goal.hpp"
 
+constexpr const char* commands =
+	"Commands:\n"
+	"apply - apply a rule of natural deduction on current goal\n"
+	"help - receive this help message\n"
+	"list - list available natural deduction rules\n"
+	"exit - stop the program\n";
+
+constexpr const char* list_of_rules =
+	"assumption\n"
+	"Introduction rules:\n"
+	"rule notI\n"
+	"rule conjI\n"
+	"rule disjI1\n"
+	"rule disjI2\n"
+	"rule impI\n"
+	"rule iffI\n"
+	"rule allI\n"
+	"rule exI\n"
+	"Elimination rules:\n"
+	"rule notE\n"
+	"rule conjE\n"
+	"rule disjE\n"
+	"rule impE\n"
+	"rule iffE\n"
+	"rule allE\n"
+	"rule exE\n";
+
 class Prover {
 	public:
 		Prover(const std::shared_ptr<Formula>& f)
