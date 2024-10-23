13019a963fd0d753d2562727365511fd194de367
RobertDoza <dozarobert98@gmail.com>
2024-10-23 07:36:21 -0700

    Add main entry point for the program
    - Added main.cpp in src/.
    - Implemented the main() function that reads formula from file and starts the interactive prover.

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <memory>
+
+#include "formula.hpp"
+#include "prover.hpp"
+
+extern std::shared_ptr<Formula> parse_formula(const std::string& filename);
+
+int main(int argc, char** argv) {
+	try {
+		std::shared_ptr<Formula> f;
+		
+		if (argc != 2) {
+			throw std::invalid_argument(std::string("Usage: ") + argv[0] + " <filename>");
+		}
+		
+		f = parse_formula("input.txt");
+		
+		Prover p(f);
+		p.start();
+	} catch (const std::exception& e) {
+		std::cerr << "Error:" << std::endl;
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	
+	return 0;
+}
+
