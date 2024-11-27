1752e6a78a791f4608bbdc65ad15aac3d799d287
Robert <dozarobert98@gmail.com>
2024-11-28 00:10:46 +0100

    Move classes & struct to command module
    - Moved Command struct to command.hpp.
    - Moved enum classes Rule and CommandType to command.hpp.

--- include/command.hpp
+++ include/command.hpp
@@ -1,1 +1,40 @@
 #pragma once
+
+#include <optional>
+
+enum class Rule {
+	Assumption,
+	NotI,
+	NotE,
+	ConjI,
+	ConjE,
+	DisjI1,
+	DisjI2,
+	DisjE,
+	ImpI,
+	ImpE,
+	IffI,
+	IffE,
+	AllI,
+	AllE,
+	ExI,
+	ExE,
+	Ccontr,
+	Classical
+};
+
+enum class CommandType {
+	Empty,
+	ListRequest,
+	HelpRequest,
+	ExitRequest,
+	RuleApplication,
+	Done,
+	Shift,
+	UnknownCommand
+};
+
+struct Command {
+	CommandType type;
+	std::optional<Rule> rule_to_apply;
+};
--- include/prover.hpp
+++ include/prover.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
-#include <optional>
-
 #include "formula.hpp"
 #include "goal.hpp"
 #include "proof_state_manager.hpp"
+#include "command.hpp"
 
 constexpr const char* commands =
 	"Commands:\n"
@@ -37,43 +36,6 @@ constexpr const char* list_of_rules =
 	"4) Rules of classical logic:\n"
 	"apply rule ccontr\n"
 	"apply rule classical";
-
-enum class Rule {
-	Assumption,
-	NotI,
-	NotE,
-	ConjI,
-	ConjE,
-	DisjI1,
-	DisjI2,
-	DisjE,
-	ImpI,
-	ImpE,
-	IffI,
-	IffE,
-	AllI,
-	AllE,
-	ExI,
-	ExE,
-	Ccontr,
-	Classical
-};
-
-enum class CommandType {
-	Empty,
-	ListRequest,
-	HelpRequest,
-	ExitRequest,
-	RuleApplication,
-	Done,
-	Shift,
-	UnknownCommand
-};
-
-struct Command {
-	CommandType type;
-	std::optional<Rule> rule_to_apply;
-};
 
 enum class ExecuteStatus {
 	Continue,
